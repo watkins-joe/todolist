5cc354f55a3d430148bd6788c2bf65bcc571440e
watkins-joe <73140971+watkins-joe@users.noreply.github.com>
2021-06-05 22:59:23 -0500

    Updated app.js
    Commented out if statement for PORT, added process.env.PORT

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
-app.listen(port, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server has started successfully.");
 });
